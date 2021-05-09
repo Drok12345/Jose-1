ee4f46c43069759832f3cd856314a5953c42c9cd
Drok12345 <83837697+Drok12345@users.noreply.github.com>
2021-05-09 14:33:40 -0500

    Add files via upload

---
+++ YO/app.js
@@ -0,0 +1,11 @@
+// botones de la nav
+$(document).ready(function(){
+    $("#menu").on("click", function(){
+       $("#menu").css("opacity", "0");
+        $("#lgMenu").addClass("enter");
+    });
+        $("#exit").on("click", function(){
+           $("#lgMenu").removeClass("enter");
+            $("#menu").css("opacity", "1");
+        });
+    });
